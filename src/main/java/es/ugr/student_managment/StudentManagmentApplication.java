52773a553bc2ba7869b45b86069716e82b7ac28e
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-14 17:26:53 +0200

    generate project with dependences rom start spring io

---
+++ src/main/java/es/ugr/student_managment/StudentManagmentApplication.java
@@ -0,0 +1,13 @@
+package es.ugr.student_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ugr/student_managment/StudentManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ugr.student_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
