1634361180e0d80e3ca03114d34e0aa86a075e00
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-15 16:40:17 +0200

    create validation for new student from

--- src/main/java/es/ugr/student_managment/controller/StudentController.java
+++ src/main/java/es/ugr/student_managment/controller/StudentController.java
@@ -2,8 +2,10 @@ package es.ugr.student_managment.controller;
 
 import es.ugr.student_managment.dto.StudentDto;
 import es.ugr.student_managment.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +34,13 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public String saveStudents(@ModelAttribute("student") StudentDto student){
+    public String saveStudents(@Valid @ModelAttribute("student") StudentDto student,
+                               BindingResult result,
+                               Model model){
+        if(result.hasErrors()){
+            model.addAttribute("student", student);
+            return "create_student";
+        }
         studentService.createStudent(student);
         return "redirect:/students";
     }
