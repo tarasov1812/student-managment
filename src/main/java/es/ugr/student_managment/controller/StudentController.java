222a26c274c37b95cd752b581a81dc241be1402d
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-15 16:00:11 +0200

    create new student feature

--- src/main/java/es/ugr/student_managment/controller/StudentController.java
+++ src/main/java/es/ugr/student_managment/controller/StudentController.java
@@ -5,6 +5,8 @@ import es.ugr.student_managment.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -27,5 +29,11 @@ public class StudentController {
         StudentDto studentDto = new StudentDto();
         model.addAttribute("student", studentDto);
         return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudents(@ModelAttribute("student") StudentDto student){
+        studentService.createStudent(student);
+        return "redirect:/students";
     }
 }
--- src/main/java/es/ugr/student_managment/service/StudentService.java
+++ src/main/java/es/ugr/student_managment/service/StudentService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface StudentService {
     List<StudentDto> getAllStudent();
+
+    void createStudent(StudentDto student);
 }
