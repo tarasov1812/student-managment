d629a85b3c01a7087125c1c03edb0d36c6939e96
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-16 10:26:27 +0200

    create view student controller

--- src/main/java/es/ugr/student_managment/controller/StudentController.java
+++ src/main/java/es/ugr/student_managment/controller/StudentController.java
@@ -74,4 +74,12 @@ public class StudentController {
         studentService.deleteStudent(studentId);
         return "redirect:/students";
     }
+
+    @GetMapping("/students/{studentId}/view")
+    public String viewStudent(@PathVariable("studentId") Long studentId,
+                              Model model){
+        StudentDto studentDto = studentService.getStudentById(studentId);
+        model.addAttribute("student", studentDto);
+        return "view_student";
+    }
 }
